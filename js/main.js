3d593d9a961e7b94ef3b59ac07997d64ec236fe3
adikiwi <atambaev.adilet@gmail.com>
2021-12-10 00:48:55 +0600

    Cards

--- js/main.js
+++ js/main.js
@@ -39,29 +39,24 @@ addForm.on("submit", addStudent);
 // ! READ
 
 let studentList = $(".students-list");
-let students = [];
 async function getStudents(API) {
   let response = await axios(API);
-  students = response.data;
-}
-
-function render(data) {
   studentList.html("");
-  data.forEach((item) => {
+  response.data.forEach((item) => {
     studentList.append(`
-    <div class="card m-3" style="width: 18rem;">
-    // <img src="${item.image}" class="card-img-top" alt="...">
+    <div class="card m-3" style="width: 19rem;">
         <div class="card-body">
-        <h5 class="card-title">${item.name}</h5>
-        <p class="card-text card-description">${item.lastname}</p>
-        <h6>Дата выпуска: ${item.number}</h6>
-        <h7>Цена: ${item.weekKpi} $</h7> <br>
+        <h4>Name:${item.name}</h4>
+        <h4>Lastname:${item.lastname}</h4>
+        <h6>Phone number: ${item.number}</h6>
+        <h7>Week KPI:: ${item.weekKpi}</h7> <br>
+        <h8>Month KPI:: ${item.monthKpi}</h8> <br>
         <a href="#" class="btn btn-primary">Show more...</a>
-        <button class="btn-delete" id="${item.id}">
-            <img src="https://cdn-icons.flaticon.com/png/512/2907/premium/2907762.png?token=exp=1638971303~hmac=b7ae44b11344da1f9f4b3227e81fcede">
+        <button class="btn btn-delete" id="${item.id}">
+            <img src="https://cdn-icons-png.flaticon.com/512/565/565491.png">
         </button>
-        <button id="${item.id}" class="btn-edit" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            <img src="https://cdn-icons-png.flaticon.com/512/747/747994.png">
+        <button id="${item.id}" class="btn btn-edit" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <img src="https://cdn-icons-png.flaticon.com/512/860/860814.png">
         </button>
     </div>
 </div>
