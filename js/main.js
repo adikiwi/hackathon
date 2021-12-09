e43dfc9dfd602541edd2f06555dd1b0ffb5bd7ae
adikiwi <atambaev.adilet@gmail.com>
2021-12-10 00:57:31 +0600

    Все скопировал с мейна

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // ! CREATE
 
-const API = "http://localhost:8000/students";
+const API = "http://localhost:3000/students";
 
 let inpName = $(".inp-name");
 let inpLastname = $(".inp-lastname");
@@ -30,38 +30,23 @@ async function addStudent(event) {
     }
   }
   let response = await axios.post(API, newStudent);
-  getStudents(API);
   console.log(response);
+  inpName.val("");
+  inpLastname.val("");
+  inpNumber.val("");
+  inpWeekKpi.val("");
+  inpMonthKpi.val("");
 }
 
 addForm.on("submit", addStudent);
 
-// ! READ
+// ! Search
+let searchInp = $(".search-inp");
 
-let studentList = $(".students-list");
-async function getStudents(API) {
-  let response = await axios(API);
-  studentList.html("");
-  response.data.forEach((item) => {
-    studentList.append(`
-    <div class="card m-3" style="width: 19rem;">
-        <div class="card-body">
-        <h4>Name:${item.name}</h4>
-        <h4>Lastname:${item.lastname}</h4>
-        <h6>Phone number: ${item.number}</h6>
-        <h7>Week KPI:: ${item.weekKpi}</h7> <br>
-        <h8>Month KPI:: ${item.monthKpi}</h8> <br>
-        <a href="#" class="btn btn-primary">Show more...</a>
-        <button class="btn btn-delete" id="${item.id}">
-            <img src="https://cdn-icons-png.flaticon.com/512/565/565491.png">
-        </button>
-        <button id="${item.id}" class="btn btn-edit" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            <img src="https://cdn-icons-png.flaticon.com/512/860/860814.png">
-        </button>
-    </div>
-</div>
-`);
-  });
+async function liveSearch(event) {
+  let value = event.target.value;
+  let newAPI = `${API}?q${value}`;
+  //!  getStudent(newAPI) - включишь когда будет функция GETSTUDENTS
 }
 
-getStudents(API);
+searchInp.on("input", liveSearch);
