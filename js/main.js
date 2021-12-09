f705b01e94ca06bec7477c2cb885c61490194709
adikiwi <atambaev.adilet@gmail.com>
2021-12-10 05:23:07 +0600

    Добавил Delete и Update

--- js/main.js
+++ js/main.js
@@ -30,6 +30,7 @@ async function addStudent(event) {
     }
   }
   let response = await axios.post(API, newStudent);
+  getStudents(API);
   console.log(response);
   inpName.val("");
   inpLastname.val("");
@@ -40,13 +41,101 @@ async function addStudent(event) {
 
 addForm.on("submit", addStudent);
 
+// ! Read
+
+let tableBody = $(".t-body");
+async function getStudents(API) {
+  let response = await axios(API);
+  tableBody.html("");
+  response.data.forEach((item) => {
+    tableBody.append(`
+    <tr>
+      <td>${item.id}</td>
+      <td>${item.name}</td>
+      <td>${item.lastname}</td>
+      <td>${item.number}</td>
+      <td>${item.weekKpi}</td>
+      <td>${item.monthKpi}</td>
+      <td><button class="btn btn-delete btn-danger delete-user" id="${item.id}">DELETE</button></td>
+      <td><button id="${item.id}" class="btn btn-edit btn-warning edit-user" data-bs-toggle="modal" data-bs-target="#exampleModal2">EDIT</button></td>
+    </tr>
+`);
+  });
+}
+
+getStudents(API);
+
+// ! Delete
+
+async function deleteStudent(event) {
+  let id = event.target.id;
+  await axios.delete(`${API}/${id}`);
+  alert("Deleted successfully");
+  getStudents(API);
+}
+
+$(document).on("click", ".btn-delete", deleteStudent);
+
+// ! Update
+
+let updateForm = $(".update-form");
+let editName = $(".edit-name");
+let editLastname = $(".edit-lastname");
+let editNumber = $(".edit-number");
+let editWeekKpi = $(".edit-week-kpi");
+let editMonthKpi = $(".edit-month-kpi");
+
+async function editStudent(event) {
+  let id = event.currentTarget.id;
+  let response = await axios(`${API}/${id}`);
+  editName.val(response.data.name);
+  editLastname.val(response.data.lastname);
+  editNumber.val(response.data.number);
+  editWeekKpi.val(response.data.weekKpi);
+  editMonthKpi.val(response.data.monthKpi);
+  console.log(event);
+  updateForm.attr("id", id);
+}
+
+$(document).on("click", ".btn-edit", editStudent);
+
+async function saveEditedStudent(event) {
+  event.preventDefault();
+  let id = event.currentTarget.id;
+  let name = editName.val();
+  let lastname = editLastname.val();
+  let number = editNumber.val();
+  let weekKpi = editWeekKpi.val();
+  let monthKpi = editMonthKpi.val();
+  let editedStudent = {
+    id,
+    name,
+    lastname,
+    number,
+    weekKpi,
+    monthKpi,
+  };
+  for (let key in editedStudent) {
+    if (!editedStudent[key]) {
+      alert("Заполните все поля!");
+      return;
+    }
+  }
+  await axios.patch(`${API}/${id}`, editedStudent);
+  getStudents(API);
+  $(".modal").modal("hide");
+}
+
+updateForm.on("submit", saveEditedStudent);
+
 // ! Search
+
 let searchInp = $(".search-inp");
 
 async function liveSearch(event) {
   let value = event.target.value;
-  let newAPI = `${API}?q${value}`;
-  //!  getStudent(newAPI) - включишь когда будет функция GETSTUDENTS
+  let newAPI = `${API}?q=${value}`;
+  !getStudents(newAPI); //- включишь когда будет функция GETSTUDENTS
 }
 
 searchInp.on("input", liveSearch);
